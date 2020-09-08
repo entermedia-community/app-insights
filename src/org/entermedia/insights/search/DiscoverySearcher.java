308dccf4ae3716b3ec49b61f6abd5b845e84887c
Mitsuo Yazawa <mitsuo@entermediadb.org>
2020-09-08 11:59:48 -0600

    Update DiscoverySearcher.java

--- src/org/entermedia/insights/search/DiscoverySearcher.java
+++ src/org/entermedia/insights/search/DiscoverySearcher.java
@@ -151,7 +151,7 @@ public class DiscoverySearcher extends BaseSearcher
 		 
 		if (yearSearch != null && !yearSearch.isEmpty()) {
 			log.info("year search detected: " + yearSearch);
-			queryUrl = "&count=" + count + "&query=updated_at:\""+ yearSearch + "\"";
+			queryUrl = "&count=" + count; // + "&query=updated_at:\""+ yearSearch + "\"";
 		} else {
 			queryUrl = "&count=" + count + "&query=" + textSearch;
 		}
